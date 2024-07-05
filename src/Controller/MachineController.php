8884eaf23d45c04732481d30bb7369d4c1e811d0
murrrkl <45383340+murrrkl@users.noreply.github.com>
2024-07-05 13:11:28 +1000

    Add files via upload

--- src/Controller/MachineController.php
+++ src/Controller/MachineController.php
@@ -3,49 +3,109 @@
 namespace App\Controller;
 
 use App\Repository\MachineRepository;
+use App\Repository\ProcessRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 use App\Service\ProcessBalancerService;
+use App\Service\MachineBalancerService;
 
 class MachineController extends AbstractController {
+    private $processRepository;
     private $machineRepository;
     private $processBalancerService;
+    private $machineBalancerService;
 
-    public function __construct(MachineRepository $machineRepository, ProcessBalancerService $processBalancerService)
-    {
+    public function __construct(MachineRepository $machineRepository, ProcessRepository $processRepository, ProcessBalancerService $processBalancerService, MachineBalancerService $machineBalancerService) {
         $this->machineRepository = $machineRepository;
+        $this->processRepository = $processRepository;
         $this->processBalancerService = $processBalancerService;
+        $this->machineBalancerService = $machineBalancerService;
     }
 
     /**
      * @Route("/machines", name="add_machine", methods={"POST"})
+     * @throws \Exception
      */
-    public function addMachine(Request $request): JsonResponse
-    {
+    public function addMachine(Request $request): JsonResponse {
         $requestData = json_decode($request->getContent(), true);
+        $machine = $this->machineRepository->create($requestData);
 
-        $machine = $this->machineRepository->createMachine($requestData);
-        $this->machineRepository->saveMachine($machine);
-
-        // $this->processBalancerService->rebalanceProcesses();
+        $this->machineBalancerService->rebalance($machine);
 
         return new JsonResponse(['message' => 'Machine added'], 201);
     }
 
     /**
      * @Route("/machines/{id}", name="delete_machine", methods={"DELETE"})
+     * @throws \Exception
      */
     public function deleteMachine($id) {
-        $machine = $this->machineRepository->findMachineById($id);
+        $machine = $this->machineRepository->find($id);
 
         if (!$machine) {
             return new JsonResponse(['message' => 'Machine not found'], 404);
         }
 
-        $this->machineRepository->removeMachine($machine);
-        // $this->processBalancerService->rebalanceProcesses();
+        $processes = $machine->getProcesses();
+        $processIds = array();
+
+        foreach ($processes as $process) {
+            $processIds[] = $process->getId();
+        }
+
+        foreach ($processes as $process) {
+            $process->setMachine(null);
+            $this->processRepository->save($process);
+        }
+
+        $machine->getProcesses()->clear();
+        $this->machineRepository->save($machine);
+        $this->machineRepository->delete($machine);;
+
+        foreach ($processIds as $processId) {
+            $process = $this->processRepository->find($processId);
+            if ($process) {
+                $this->processBalancerService->rebalance($process);
+            }
+        }
+
         return new JsonResponse(['message' => 'Machine deleted'], 200);
+    }
+
+    /**
+     * @Route("/machines", name="get_machines", methods={"GET"})
+     */
+    public function getAllMachinesInfo() : JsonResponse {
+        $machines = $this->machineRepository->findAll();
+
+        $data = [];
+        foreach ($machines as $machine) {
+            $data[$machine->getId()] = [
+                'totalMemory' => $machine->getTotalMemory(),
+                'totalProcessor' => $machine->getTotalProcessor(),
+                'freeMemory' => $machine->getFreeMemory(),
+                'freeProcessor' => $machine->getFreeProcessor(),
+                'numberOfProcesses' => $machine->getNumberOfProcesses(),
+                'processes' => []
+            ];
+
+            $machine_processes = $machine->getProcesses();
+
+            if (!empty($machine_processes)) {
+                foreach ($machine_processes as $process) {
+                    $data[$machine->getId()]['processes'][] = [
+                        'id' => $process->getId(),
+                        'requiredMemory' => $process->getRequiredMemory(),
+                        'requiredProcessor' => $process->getRequiredProcessor()
+                    ];
+                }
+            }
+        }
+
+        return new JsonResponse($data);
     }
 }
--- src/Controller/ProcessesController.php
+++ src/Controller/ProcessesController.php
@@ -2,50 +2,88 @@
 
 namespace App\Controller;
 
+use App\Repository\MachineRepository;
 use App\Repository\ProcessRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 use App\Service\ProcessBalancerService;
+use App\Service\MachineBalancerService;
 
 class ProcessesController extends AbstractController {
     private $processRepository;
+    private $machineRepository;
     private $processBalancerService;
+    private $machineBalancerService;
 
-    public function __construct(ProcessRepository $processRepository, ProcessBalancerService $processBalancerService)
-    {
+    public function __construct(MachineRepository $machineRepository, ProcessRepository $processRepository, ProcessBalancerService $processBalancerService, MachineBalancerService $machineBalancerService) {
+        $this->machineRepository = $machineRepository;
         $this->processRepository = $processRepository;
         $this->processBalancerService = $processBalancerService;
+        $this->machineBalancerService = $machineBalancerService;
     }
 
     /**
      * @Route("/processes", name="add_process", methods={"POST"})
      */
-    public function addProcess(Request $request)
-    {
+    public function addProcess(Request $request) {
         $requestData = json_decode($request->getContent(), true);
+        $process = $this->processRepository->create($requestData);
 
-        $process = $this->processRepository->createProcess($requestData);
-        $this->processRepository->saveProcess($process);
+        $this->processBalancerService->rebalance($process);
 
-        // $this->processBalancerService->rebalanceProcesses();
         return new JsonResponse(['message' => 'Process added'], 201);
     }
 
     /**
      * @Route("/processes/{id}", name="delete_process", methods={"DELETE"})
+     * @throws \Exception
      */
-    public function deleteProcess($id)
-    {
-        $process = $this->processRepository->findProcessById($id);
+    public function deleteProcess($id) {
+        $process = $this->processRepository->find($id);
 
         if (!$process) {
             return new JsonResponse(['message' => 'Process not found'], 404);
         }
 
-        $this->processRepository->removeProcess($process);
-        // $this->processBalancerService->rebalanceProcesses();
+        $machine = $process->getMachine();
+
+        if ($machine !== null) {
+            $process = $this->processRepository->findOneByIdJoinedToMachine($id);
+            $machine = $process->getMachine();
+            $machine->freeingUpResources($process);
+
+            $this->machineRepository->save($machine);
+            $machine->removeProcess($process);
+
+            $this->machineBalancerService->rebalance($machine);
+        }
+
+        $this->processRepository->delete($process);
+
         return new JsonResponse(['message' => 'Process deleted'], 200);
     }
+
+    /**
+     * @Route("/processes", name="get_processes", methods={"GET"})
+     */
+    public function getAllProcessesInfo() : JsonResponse {
+        $processes = $this->processRepository->findAll();
+
+        $data = [];
+        foreach ($processes as $process) {
+            $machineId = $process->getMachine()?->getId();
+
+            $data[$process->getId()] = [
+                'requiredMemory' => $process->getRequiredMemory(),
+                'requiredProcessor' => $process->getRequiredProcessor(),
+                'machine_num' => $machineId,
+            ];
+        }
+        return new JsonResponse($data);
+    }
+
 }
