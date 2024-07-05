c0831446878a1c98ac3c503c7ccfa52294aa6e9a
murrrkl <45383340+murrrkl@users.noreply.github.com>
2024-07-05 13:12:43 +1000

    Add files via upload

--- src/Repository/MachineRepository.php
+++ src/Repository/MachineRepository.php
@@ -2,37 +2,116 @@
 
 namespace App\Repository;
 
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Machine;
 
-class MachineRepository extends ServiceEntityRepository {
-    private $entityManager;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Exception;
 
-    public function __construct(ManagerRegistry $registry) {
+/**
+ * @extends ServiceEntityRepository<Machine>
+ *
+ * @method Machine|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Machine|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Machine[]    findAll()
+ * @method Machine[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MachineRepository extends ServiceEntityRepository
+{
+    private $entityManager;
+    public function __construct(ManagerRegistry $registry)
+    {
         parent::__construct($registry, Machine::class);
         $this->entityManager = $registry->getManager();
     }
 
-    public function createMachine(array $requestData): Machine {
-        $machine = new Machine();
-        $machine->setTotalMemory($requestData['total_memory'])->setTotalProcessor(['total_processor'])->setNumberOfProcesses(0);
+    /**
+     * @throws Exception
+     */
+    public function create(array $requestData): Machine {
+        $machine = new Machine($requestData['total_memory'], $requestData['total_processor']);
+        $this->save($machine);
 
         return $machine;
     }
 
-    public function saveMachine(Machine $machine): void {
-        $this->entityManager->persist($machine);
-        $this->entityManager->flush();
+    /**
+     * @throws Exception
+     */
+    public function save(Machine $machine): void {
+        $this->entityManager->beginTransaction();
+        try {
+            $this->entityManager->persist($machine);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
 
-    public function findMachineById($id) {
-        return $this->find($id);
+    /**
+     * @throws \Exception
+     */
+    public function delete(Machine $machine): void {
+        $this->entityManager->beginTransaction();
+        try {
+            $this->entityManager->remove($machine);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
 
-    public  function  removeMachine(Machine $machine) : void {
-        $this->entityManager->remove($machine);
-        $this->entityManager->flush();
+    public function findByIsHighLoad($processLimit) {
+        return $this->createQueryBuilder('m')
+            ->where('m.numberOfProcesses > :processLimit')
+            ->setParameter('processLimit', $processLimit)
+            ->getQuery()
+            ->getResult();
     }
+
+    public function findByHasSufficientProcesses($processLimit) {
+        return $this->createQueryBuilder('m')
+            ->where('m.numberOfProcesses = :processLimit')
+            ->setParameter('processLimit', $processLimit)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findByHasInsufficientProcesses($processLimit) {
+        return $this->createQueryBuilder('m')
+            ->where('m.numberOfProcesses < :processLimit')
+            ->setParameter('processLimit', $processLimit)
+            ->getQuery()
+            ->getResult();
+    }
+
+
+//    /**
+//     * @return Machine[] Returns an array of Machine objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('m')
+//            ->andWhere('m.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('m.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Machine
+//    {
+//        return $this->createQueryBuilder('m')
+//            ->andWhere('m.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
 }
--- src/Repository/ProcessRepository.php
+++ src/Repository/ProcessRepository.php
@@ -2,12 +2,23 @@
 
 namespace App\Repository;
 
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Process;
 
-class ProcessRepository extends ServiceEntityRepository {
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\Persistence\ManagerRegistry;
+use Exception;
+
+/**
+ * @extends ServiceEntityRepository<Process>
+ *
+ * @method Process|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Process|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Process[]    findAll()
+ * @method Process[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ProcessRepository extends ServiceEntityRepository
+{
     private $entityManager;
 
 
@@ -16,24 +27,89 @@ class ProcessRepository extends ServiceEntityRepository {
         $this->entityManager = $registry->getManager();
     }
 
-    public function createProcess(array $requestData): Process {
-        $machine = new Process();
-        $machine->setRequiredMemory($requestData['require_memory'])->setRequiredProcessor(['require_processor']);
+    /**
+     * @throws Exception
+     */
+    public function create(array $requestData): Process {
+        $processor = new Process($requestData['required_memory'], $requestData['required_processor']);
+        $this->save($processor);
 
-        return $machine;
+        return $processor;
     }
 
-    public function saveProcess(Process $process): void {
-        $this->entityManager->persist($process);
-        $this->entityManager->flush();
+    /**
+     * @throws Exception
+     */
+    public function save(Process $process): void {
+        $this->entityManager->beginTransaction();
+        try {
+            $this->entityManager->persist($process);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
 
-    public function findProcessById($id) {
-        return $this->find($id);
+    /**
+     * @throws \Exception
+     */
+    public function delete(Process $process): void {
+        $this->entityManager->beginTransaction();
+        try {
+            $this->entityManager->remove($process);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
 
-    public  function  removeProcess(Process $process) : void {
-        $this->entityManager->remove($process);
-        $this->entityManager->flush();
+    public function findByWaitingForMachine() {
+        return $this->createQueryBuilder('p')
+            ->where('p.machine IS NULL')
+            ->getQuery()
+            ->getResult();
     }
+
+    /**
+     * @throws NonUniqueResultException
+     */
+    public function findOneByIdJoinedToMachine(int $processId): ?Process {
+        $query = $this->entityManager->createQuery(
+            'SELECT p, c
+            FROM App\Entity\Process p
+            INNER JOIN p.machine c
+            WHERE p.id = :id'
+        )->setParameter('id', $processId);
+
+        return $query->getOneOrNullResult();
+    }
+
+//    /**
+//     * @return Process[] Returns an array of Process objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Process
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
 }
